d5e7431b3a0f72c28c94f2a347e8e9698f00e7f4
Dickson <dicmora2@gmail.com>
2025-05-03 18:12:03 -0400

    se_project_spots

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,28 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const profileFormElement = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescription = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const profileNameElement = document.querySelector(".profile__name");
+const profileDescriptionElement = document.querySelector(
+  ".profile__description"
+);
+const newPostFormElement = newPostModal.querySelector(".modal__form");
+const newPostInput = newPostModal.querySelector("#card-caption-input");
+const postLinkInput = newPostModal.querySelector("#card-image-input");
 
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameElement.textContent;
+  editProfileDescription.value = profileDescriptionElement.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -20,3 +37,32 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleProfileFormSubmit(evt) {
+  // Prevent default browser behavior.
+  evt.preventDefault();
+
+  // Insert these new values into the textContent
+  // property of the corresponding profile elements.
+  profileNameElement.textContent = editProfileNameInput.value;
+  profileDescriptionElement.textContent = editProfileDescription.value;
+
+  editProfileModal.classList.remove("modal_is-opened"); // Close the modal.
+}
+// Set the submit listener.
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+// Create the form submission handler.
+function handleNewPostSubmit(evt) {
+  // Prevent default browser behavior.
+  evt.preventDefault();
+
+  // Log both input values to the console.
+  console.log(newPostInput.value);
+  console.log(postLinkInput.value);
+
+  newPostModal.classList.remove("modal_is-opened"); // Close the modal.
+}
+
+// Create the submit listener.
+newPostFormElement.addEventListener("submit", handleNewPostSubmit);
